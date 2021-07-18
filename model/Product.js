fff90c5d4e56956b3056014b8a4b3138c23a2fef
austine <austineatori@gmail.com>
2021-07-18 04:04:56 +0100

    austine

--- model/Product.js
+++ model/Product.js
@@ -1,84 +1,43 @@
 let products = [
     {
         id: 1,
-        name: "Hyundai i10",
-        description: "Small, economical, practical and super easy to drive.",
-        image: "https://www.carstore.com/-/media/carstore/makes/hyundai/i10/hyundai-i10-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=783C00C261E828EA47B2DDCC4DF01DBC",
-        price: 2200000
+        name: "laptop",
+        description: "one terrabite, four ghz, smart, efficient.",
+        image: "https://www.google.com/search?q=laptop++picture&sxsrf=ALeKk02RaGZFoPZwI8jPKEtgFgY01m1VFw:1626539794512&tbm=isch&source=iu&ictx=1&fir=0vRBGtHIRtMF9M%252CcMfgFMspuOTiHM%252C_&vet=1&usg=AI4_-kQeH8yeCqLfdKKWGwQBWkxqn3ga5w&sa=X&ved=2ahUKEwi4wKj1xOrxAhXF2KQKHRTMA-kQ9QF6BAgQEAE#imgrc=0vRBGtHIRtMF9M", 
+        price: 500000
     },
 
     {
         id: 2,
-        name: "Honda Civic",
-        description: "A popular hatchback with futuristic and striking style.",
-        image: "https://www.carstore.com/-/media/carstore/makes/honda/civic/honda-civic-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=1AC266853C61DDB67E412DAFCA96CAFE",
-        price: 1500000
+        name: "iPhone12",
+        description: "Classic, stylish, sleek.",
+        image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT_wv5bDLGVB9RwDtVKdF49MzYvmr6FoIvf1oN5Lm8uMjOweSESG_YATgQZU1OZd6wKEJsaCwnXYWAv&usqp=CAc",
+        price: 800000
     },
 
     {
         id: 3,
-        name: "Ford Mondeo",
-        description: "A textbook family car with solid build quality and multi award-winning engines.",
-        image: "https://www.carstore.com/-/media/carstore/makes/ford/mondeo/ford-mondeo-front-thumbnail-480x270.ashx?mh=519&la=en&h=290&w=500&mw=519&hash=879EC4B92C6F4B7361D32B7F76C6337F",
-        price: 1800000
+        name: "Powerbank",
+        description: "A ten thousand mHA Battery life, 40 hours standby time",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.mobisun.com%2Fwp-content%2Fuploads%2F2018%2F09%2FMacBook-lader-power-bank-Mobisun-20000-mah-PD-45W-Power-Delivery-Charger-iPhone-X-8-Nexus-5X-6P-800x800.jpg&imgrefurl=https%3A%2F%2Fwww.mobisun.com%2Fblog%2F10-tips-for-buying-a-power-bank%2F&tbnid=syo2_HWA97EG5M&vet=12ahUKEwjum_K-yurxAhUQx7sIHa30CHkQMygBegUIARDkAQ..i&docid=heqJnSIF8njdCM&w=800&h=800&q=power%20bank&hl=en&ved=2ahUKEwjum_K-yurxAhUQx7sIHa30CHkQMygBegUIARDkAQ",
+        price: 15000
     },
 
     {
         id: 4,
-        name: "Fiat 500",
-        description: "Curvy, cute and compact, a fantastic city car.",
-        image: "https://www.carstore.com/-/media/carstore/makes/fiat/500/fiat-500-red-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=BF55D2EE4EBEDA8FA30C11D071B7A93A",
-        price: 2000000
+        name: "Desktop",
+        description: "smart, affordable, WiFi enabled, Five hundred MB.",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.saymedia-content.com%2F.image%2Ft_share%2FMTc0NDY0NTMyOTQzNDgwNDU0%2Fbuying-your-first-desktop-computer.jpg&imgrefurl=https%3A%2F%2Fturbofuture.com%2Fcomputers%2FBuying-Your-First-Desktop-Computer&tbnid=dQnR2eexVBcWdM&vet=12ahUKEwiX78nuyerxAhV9wbsIHTr8AZUQMygBegUIARDjAQ..i&docid=4FzWCdTmHb3-DM&w=1200&h=736&q=computer&hl=en&ved=2ahUKEwiX78nuyerxAhV9wbsIHTr8AZUQMygBegUIARDjAQ",
+        price: 80000
     },
 
     {
         id: 5,
-        name: "Range Rover",
-        description: "A premium SUV offering the best comfort on the road.",
-        image: "https://www.carstore.com/-/media/carstore/makes/land-rover/range-rover/land-rover-blue-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=A592FC91A28123826389EF9E4BD6B7D6",
-        price: 5000000
+        name: "Flashdrive",
+        description: "Smart pendrive with inbuilt antivirus.",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.computerhope.com%2Fcdn%2Fflash-drive.png&imgrefurl=https%3A%2F%2Fwww.computerhope.com%2Fjargon%2Fj%2Fjumpdriv.htm&tbnid=ZawdTN_kwI1miM&vet=12ahUKEwibiLWry-rxAhU147sIHVxsCXwQMygEegUIARDmAQ..i&docid=tDQlwcBcF0FH6M&w=350&h=350&q=flash%20drive&hl=en&ved=2ahUKEwibiLWry-rxAhU147sIHVxsCXwQMygEegUIARDmAQ",
+        price: 14000
     },
-
-    {
-        id: 6,
-        name: "Kia Sportage",
-        description: "A popular SUV with lots of equipment as standard.",
-        image: "https://www.carstore.com/-/media/carstore/makes/kia/sportage/kia-sportage-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=F1FFE82ADAEEEEE9DB4A43118D104109",
-        price: 3500000
-    },
-
-    {
-        id: 7,
-        name: "Nissan Micra",
-        description: "A superbly reliable, easy to drive and cheap to run supermini.",
-        image: "https://www.carstore.com/-/media/carstore/makes/nissan/micra/high-res-images/nissian-micra-thumbnail-480x270px.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=FF11119152082C846A49FD03D03696A2",
-        price: 1000000
-    },
-
-    {
-        id: 8,
-        name: "Audi A1",
-        description: "A claasy supermini with a fresh sporty design.",
-        image: "https://www.carstore.com/-/media/carstore/makes/audi/a1/audi-a1-grey-front-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=A889989AA131BBA5E4ED0227B0F887F1",
-        price: 1800000
-    },
-
-    {
-        id: 9,
-        name: "BMW X1",
-        description: "Meet the smallest of the SUV range. Comfortable, good looking yet engaging to drive.",
-        image: "https://www.carstore.com/-/media/carstore/makes/bmw/x1/bmw-x1-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=D30BBC7BCE77D0C4E5D0B13537038A16",
-        price: 900000
-    },
-
-    {
-        id: 10,
-        name: "Toyota Aygo",
-        description: "A great city car that is good-looking, and comfortable to drive.",
-        image: "https://www.carstore.com/-/media/carstore/makes/toyota/aygo/toyota-aygo-front-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=CFA7FE7F9BBBE18561257BD9108E3589",
-        price: 6000000
-    }
-
 
 ];
 
